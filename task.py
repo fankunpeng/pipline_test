9e97e7a44e5c4faaf94d5f1ec4b8c4f8f0cdd63c
fankunpeng <fankunpeng@kuainiujinke.com>
2018-01-11 14:18:58 +0800

    in master3

--- task.py
+++ task.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # test
 @app.route('/')
 def hello_world():
-    return json.dumps('again in master2')
+    return json.dumps('again in master3')
 
 
 if __name__ == '__main__':
