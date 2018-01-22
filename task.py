3c7f887458f4163728d7af17ceb2a81eb4a9cf22
fankunpeng <fankunpeng@kuainiujinke.com>
2018-01-22 17:39:23 +0800

    master 第八次构建

--- task.py
+++ task.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # test
 @app.route('/')
 def hello_world():
-    return json.dumps('again in master4')
+    return json.dumps('again in master5')
 
 
 if __name__ == '__main__':
